477cb172b6b9f7d22859049f57ce38f9e94e13a8
Jimmy Ostler <jtostler1@gmail.com>
2023-03-10 14:19:49 -0800

    Added template added day 2 reformatted day 1

--- day 1/day1.js
+++ day 1/day1.js
@@ -1,14 +1,14 @@
-console.log("Begin readline program");
+console.log("Begin File Input");
 
 const fs = require('fs');
 const filename = 'input.txt';
 
 const input = fs.readFileSync(filename, 'utf-8');
 
+const integers = input.split('\n').map(Number);
+
 var list = []
 var temp = 0;
-
-const integers = input.split('\n').map(Number);
 
 console.log("Begin Computation")
 for (var i = 0; i < integers.length; ++i) {
@@ -51,4 +51,15 @@ for (var i = 0; i < list.length; ++i) {
 
 console.log(max);
 
+list[list.indexOf(max)] = 0;
 
+max = 0;
+for (var i = 0; i < list.length; ++i) {
+    if (list[i] > max) {
+        max = list[i];
+    }
+}
+
+console.log(max);
+
+64929+64690+64078
