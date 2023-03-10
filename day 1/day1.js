8496dbb73be86e77a06ce4ccbab05b0e1febacd4
Jimmy Ostler <jtostler1@gmail.com>
2023-03-10 14:11:21 -0800

    Day 1, part 2

--- day 1/day1.js
+++ day 1/day1.js
@@ -29,4 +29,26 @@ for (var i = 0; i < list.length; ++i) {
 
 console.log(max);
 
+list[list.indexOf(max)] = 0;
+
+max = 0;
+for (var i = 0; i < list.length; ++i) {
+    if (list[i] > max) {
+        max = list[i];
+    }
+}
+
+console.log(max);
+
+list[list.indexOf(max)] = 0;
+
+max = 0;
+for (var i = 0; i < list.length; ++i) {
+    if (list[i] > max) {
+        max = list[i];
+    }
+}
+
+console.log(max);
+
 
