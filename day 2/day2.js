c9e5dec3c46e9de4dacaab795fc77f3d78183e69
Jimmy Ostler <jtostler1@gmail.com>
2023-03-10 14:50:25 -0800

    Day 2 part 1 done

--- day 2/day2.js
+++ day 2/day2.js
@@ -5,4 +5,84 @@ const filename = 'input.txt';
 
 const input = fs.readFileSync(filename, 'utf-8');
 
-const integers = input.split('\n').map(Number);
+const round = input.split('\n');
+
+const theirChoice = {
+    ROCK: "A",
+    PAPER: "B",
+    SCISSORS: "C"
+}
+
+const myChoice = {
+    ROCK: "X",
+    PAPER: "Y",
+    SCISSORS: "Z"
+}
+
+var score  = 0
+
+function win(theirs, mine) {
+    if (theirs == theirChoice.ROCK) {
+        switch (mine) {
+            case "X":
+                return "draw";
+            case "Y":
+                return "win";
+            case "Z":
+                return "loss";
+            default:
+                console.log("error");
+        }
+    } else if (theirs == theirChoice.PAPER) {
+        switch (mine) {
+            case "X":
+                return "loss";
+            case "Y":
+                return "draw";
+            case "Z":
+                return "win";
+            default:
+                console.log("error");
+        }
+    } else if (theirs == theirChoice.SCISSORS) {
+        switch (mine) {
+            case "X":
+                return "win";
+            case "Y":
+                return "loss";
+            case "Z":
+                return "draw";
+            default:
+                console.log("error");
+        }
+    }
+}
+
+for (var i = 0; i < round.length; ++i) {
+    switch (win(round[i].at(0), round[i].at(2))) {
+        case "win":
+            score += 6;
+            break;
+        case "draw": 
+            score += 3;
+            break;
+        case "loss":
+            score += 0;
+            break;
+    }
+    switch (round[i].at(2)) {
+        case "X":
+            score += 1;
+            break;
+        case "Y":
+            score += 2;
+            break;
+        case "Z":
+            score += 3;
+            break;
+        default:
+            console.log("error");
+    }
+}
+
+console.log(score)
